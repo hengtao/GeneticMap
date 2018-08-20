5a7e0f3b7e6cbf22b23bb0df932b13172797903b
hengtao <hengtaostar@163.com>
2018-08-20 18:48:14 +0800

    Update QTLGene-databaseAnnot.py

--- QTLGene-databaseAnnot.py
+++ QTLGene-databaseAnnot.py
@@ -9,6 +9,11 @@ import configparser
 from functools import reduce
 import logging
 from datetime import datetime
+import http.cookiejar
+import urllib.request
+import urllib.parse
+import urllib2
+from bs4 import BeautifulSoap as bs
 
 class HelpFormatter(argparse.RawDescriptionHelpFormatter, argparse.ArgumentDefaultsHelpFormatter):
     pass
@@ -66,22 +71,61 @@ def Cmd_and_Time(cmd, logger):
         logger.info("Total time for this step is: {days} days, {hours} hours, {minites} minites, {seconds} seconds".format(days = days, hours = hours, minites = minites, seconds = seconds))
     return spend_time
 
-def EvidenceLevel(args):
+def EvidenceLevel(evidence, queryvcf, outputfile):
 	FuncrefGene = re.compile(r"Func.refGene=(.*?);")
 	GenerefGene = re.compile(r"Gene.refGene=(.*?);")
-	evidence = args.evidence
-	queryvcf = args.queryvcf
+	ExonicFuncrefGene = re.compile(r"ExonicFunc.refGene=(.*?);")
+	AAChangerefGene   = re.compile(r"AAChange.refGene=(.*?);")
+	condidategenes = list()
+	o = open(outputfile, 'w+')
 	with open(queryvcf) as f:
 		for line in f:
-			
-	
+			contentlist = line.strip().split("\t")	
+			chrom = contentlist[0]
+			posit = contentlist[1]
+			refer = contentlist[3]
+			alter = contentlist[4]
+			infor = contentlist[7]
+			MatchFuncrefGene = re.findall(FuncrefGene, infor)[1]
+			MatchGenerefGene = re.findall(GenerefGene, infor)[1]
+			MatchExonicFuncrefGene = re.findall(ExonicFuncrefGene, infor)[1]
+			MatchAAChangerefGene   = re.findall(AAChangerefGene, infor)[1]
+			if MatchFuncrefGene in evidence:
+				o.write(MatchGenerefGene + "\t" + chrom + "\t" + posit + "\t" + refer + "\t" + alter + "\t" + MatchFuncrefGene + "\t" + MatchExonicFuncrefGene + "\t" +  MatchAAChangerefGene + "\n")
+			if MatchFuncrefGene == "Exonic":
+				if MatchExonicFuncrefGene == "nonsynonymous_SNV" or MatchExonicFuncrefGene.startswith("stop") or MatchExonicFuncrefGene.startswith("frameshift"):
+					if MatchGenerefGene not in condidategenes:
+						condidategenes.append(MatchGenerefGene)
+	o.close()
+	return MatchGenerefGene
+						
 def VersionConvert(args):
 	versionFile = args.versionFile
-	querygene = args.querygene
-	with open(querygene, 'r') as f:
+	evidence = args.evidence
+	queryvcf = args.queryvcf
+	outputfile = args.outputfile
+	versoionname = args.versionName
+	genenamefile = args.genenamefile
+	querygene  = EvidenceLevel(evidence, queryvcf, outputfile)
+	condidategenes = list()
+	targetgenes = list()
+	o = open(querygene, 'w+')
+	with open(versionFile, 'r') as f:
 		for line in f:
-			
-	
+			for gene in querygene:
+				if re.findall(gene, line):
+					del(querygene[0])
+					linecontents = line.strip().split("\t")[1:]
+					lineversionname  = [content.split("|")[1] if content.startswith(versoionname) for content in linecontents]
+					linewithoutname  = [content.split("|")[1] if not content.startswith(versoionname) for content in linecontents]	
+					if gene in lineversionname:
+						condidategenes.extend(linewithoutname)
+						o.write(gene + "\t" + ",".join(linewithoutname))
+	o.close()
+	for gene in condidategenes:
+		if gene not in targetgenes:
+			targetgenes.append(gene)
+	return targetgenes
 	
 def main(args):
     logfile  = args.logfile
@@ -102,13 +146,14 @@ def main(args):
     ## 指定日志的最低输出级别，默认为WARN
     logger.setLevel(level = logging.INFO)
 
-    gatk     = Check_software(logger, "GenomeAnalysisTK.jar")
-    vcftools = Check_software(logger, "vcftools1")
-    beagle   = Check_software(logger, "beagle.jar")
-    plink    = Check_software(logger, "plink")
-
+	evidence = args.evidence
+	queryvcf = args.queryvcf
+	outputfile = args.outputfile
+	
     if args.versionFile:
-		VersionConvert(args)
+		targetgenes = VersionConvert(args)
+	else:
+		targetgenes = EvidenceLevel(evidence, queryvcf, outputfile)
 
 if __name__ == "__main__":
     scriptpath = os.path.split(os.path.realpath(__file__))[0]
@@ -120,11 +165,16 @@ This script was used to filterout SNPs that failed to pass GATK criterion and wi
 '''.format(scriptpath = scriptpath))
 
     parse.add_argument('-query', '--queryvcf', required = True, dest = "queryvcf", help = "vcf file without intergenic regions info", metavar = "vcf file without intergenic regions info", type = str, nargs = '?')
-    parse.add_argument('-logfile', '--logfile', required = True, dest = "logfile", help = "Log file to record procedures of processing of this script", metavar = "Log file to record procedures of processing of this script", type = str, nargs = '?')
+    parse.add_argument('-outfile', '--out', required = True, dest = "outputfile", help = "outputfile record condidates SNP/Indel", metavar = "outputfile record condidates SNP/Indel", type = str, nargs = '?')
+    parse.add_argument('-genenamefile', '--namefile', required = True, dest = "genefile", help = "condidate genes with their corresponding names of another version", type = str, nargs = '?')
+
+	parse.add_argument('-logfile', '--logfile', required = True, dest = "logfile", help = "Log file to record procedures of processing of this script", metavar = "Log file to record procedures of processing of this script", type = str, nargs = '?')
 	parse.add_argument('-evidence', '--evidence', required = True, dest = "evidence", default = "exonic downstream intronic splicing upstream UTR3 UTR5", 
 					   help = "Positon where SNP/indel located in will be further studied", type = str, nargs = '*')
-
-	parse.add_argument('-versionFile', '--versionFile', required = False, dest = "versionFile", help = "Responding relationship between two versions", type = str, nargs = '?')
-    args = parse.parse_args()
+	versiongroup = parse.add_argument_group("verion conversion")
+	versiongroup.add_argument('-versionFile', '--versionFile', required = False, dest = "versionFile", help = "Responding relationship between two versions", type = str, nargs = '?')
+	versiongroup.add_argument('-versionname', '--versionname', required = False, dest = "versionName", help = "version name should be consistent with that in verionfile", type = str, nargs = '?')
+	
+	args = parse.parse_args()
 
     main(args)
